ed0676c10556812cc226fda250fcccb9b8b1ffa2
Roman Medvedev <romankazan88@gmail.com>
2021-10-01 13:15:10 +0300

    MT

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        long price = 2_000_000_000;
-        long count = 500_000_000;
-        long total = price + count;
+        int balance = 2_000_000_000;
+        int  sum = 500_000_000;
+        int total = balance + sum ;
         System.out.println(total);
     }
 }
